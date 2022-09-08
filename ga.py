2fe06199547724b2c705fcae2d0643cc2997f30e
yoggarts <yoggarts@gmail.com>
2022-09-08 15:51:37 +0700

    Add files via upload

---
+++ ga.py
@@ -0,0 +1,179 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+import math, sys
+import random
+from util import City, read_cities, write_cities_and_return_them, generate_cities
+np.warnings.filterwarnings('ignore', category=np.VisibleDeprecationWarning)   
+
+class Fitness:
+    def __init__(self, route):
+        self.route = route
+        self.distance = 0
+        self.fitness = 0.0
+
+    def path_cost(self):
+        if self.distance == 0:
+            distance = 0
+            for index, city in enumerate(self.route):
+                distance += city.distance(self.route[(index + 1) % len(self.route)])
+            self.distance = distance
+        return self.distance
+
+    def path_fitness(self):
+        if self.fitness == 0:
+            self.fitness = 1 / float(self.path_cost())
+        return self.fitness
+
+
+class GeneticAlgorithm:
+    def __init__(self, iterations, population_size, cities, elites_num, mutation_rate,
+                 greedy_seed=0, roulette_selection=True, plot_progress=True):
+        self.plot_progress = plot_progress
+        self.roulette_selection = roulette_selection
+        self.progress = []
+        self.mutation_rate = mutation_rate
+        self.cities = cities
+        self.elites_num = elites_num
+        self.iterations = iterations
+        self.population_size = population_size
+        self.greedy_seed = greedy_seed
+
+        self.population = self.initial_population()
+        self.average_path_cost = 1
+        self.ranked_population = None
+
+    def best_chromosome(self):
+        return self.ranked_population[0][0]
+
+    def best_distance(self):
+        return 1 / self.ranked_population[0][1]
+
+    def random_route(self):
+        return random.sample(self.cities, len(self.cities))
+
+    def initial_population(self):
+        p1 = [self.random_route() for _ in range(self.population_size - self.greedy_seed)]
+        greedy_population = [greedy_route(start_index % len(self.cities), self.cities)
+                             for start_index in range(self.greedy_seed)]
+        return [*p1, *greedy_population]
+
+    def rank_population(self):
+        fitness = [(chromosome, Fitness(chromosome).path_fitness()) for chromosome in self.population]
+        self.ranked_population = sorted(fitness, key=lambda f: f[1], reverse=True)
+
+    def selection(self):
+        selections = [self.ranked_population[i][0] for i in range(self.elites_num)]
+        if self.roulette_selection:
+            df = pd.DataFrame(np.array(self.ranked_population), columns=["index", "fitness"])
+            self.average_path_cost = sum(1 / df.fitness) / len(df.fitness)
+            df['cum_sum'] = df.fitness.cumsum()
+            df['cum_perc'] = 100 * df.cum_sum / df.fitness.sum()
+
+            for _ in range(0, self.population_size - self.elites_num):
+                pick = 100 * random.random()
+                for i in range(0, len(self.ranked_population)):
+                    if pick <= df.iat[i, 3]:
+                        selections.append(self.ranked_population[i][0])
+                        break
+        else:
+            for _ in range(0, self.population_size - self.elites_num):
+                pick = random.randint(0, self.population_size - 1)
+                selections.append(self.ranked_population[pick][0])
+        self.population = selections
+
+    @staticmethod
+    def produce_child(parent1, parent2):
+        gene_1 = random.randint(0, len(parent1))
+        gene_2 = random.randint(0, len(parent1))
+        gene_1, gene_2 = min(gene_1, gene_2), max(gene_1, gene_2)
+        child = [parent1[i] for i in range(gene_1, gene_2)]
+        child.extend([gene for gene in parent2 if gene not in child])
+        return child
+
+    def generate_population(self):
+        length = len(self.population) - self.elites_num
+        children = self.population[:self.elites_num]
+        for i in range(0, length):
+            child = self.produce_child(self.population[i],
+                                       self.population[(i + random.randint(1, self.elites_num)) % length])
+            children.append(child)
+        return children
+
+    def mutate(self, individual):
+        for index, city in enumerate(individual):
+            if random.random() < max(0, self.mutation_rate):
+                sample_size = min(min(max(3, self.population_size // 5), 100), len(individual))
+                random_sample = random.sample(range(len(individual)), sample_size)
+                sorted_sample = sorted(random_sample,
+                                       key=lambda c_i: individual[c_i].distance(individual[index - 1]))
+                random_close_index = random.choice(sorted_sample[:max(sample_size // 3, 2)])
+                individual[index], individual[random_close_index] = \
+                    individual[random_close_index], individual[index]
+        return individual
+
+    def next_generation(self):
+        self.rank_population()
+        self.selection()
+        self.population = self.generate_population()
+        self.population[self.elites_num:] = [self.mutate(chromosome)
+                                             for chromosome in self.population[self.elites_num:]]
+
+    def run(self):
+        if self.plot_progress:
+            plt.ion()
+        for ind in range(0, self.iterations):
+            self.next_generation()
+            self.progress.append(self.best_distance())
+            if self.plot_progress and ind % 10 == 0:
+                self.plot()
+            elif not self.plot_progress and ind % 10 == 0:
+                print(self.best_distance())
+
+    def plot(self):
+        print(self.best_distance())
+        fig = plt.figure(0)
+        plt.plot(self.progress, 'g')
+        fig.suptitle('genetic algorithm generations')
+        plt.ylabel('Distance')
+        plt.xlabel('Generation')
+
+        x_list, y_list = [], []
+        for city in self.best_chromosome():
+            x_list.append(city.x)
+            y_list.append(city.y)
+        x_list.append(self.best_chromosome()[0].x)
+        y_list.append(self.best_chromosome()[0].y)
+        fig = plt.figure(1)
+        fig.clear()
+        fig.suptitle('genetic algorithm TSP')
+        plt.plot(x_list, y_list, 'ro')
+        plt.plot(x_list, y_list, 'g')
+
+        if self.plot_progress:
+            plt.draw()
+            plt.pause(0.05)
+        plt.show()
+
+
+def greedy_route(start_index, cities):
+    unvisited = cities[:]
+    del unvisited[start_index]
+    route = [cities[start_index]]
+    while len(unvisited):
+        index, nearest_city = min(enumerate(unvisited), key=lambda item: item[1].distance(route[-1]))
+        route.append(nearest_city)
+        del unvisited[index]
+    return route
+
+
+if __name__ == '__main__':
+    cities = read_cities(64)
+    genetic_algorithm = GeneticAlgorithm(cities=cities, iterations=400, population_size=100,
+                                         elites_num=20, mutation_rate=0.008, greedy_seed=1,
+                                         roulette_selection=True, plot_progress=True)
+    genetic_algorithm.run()
+    print(genetic_algorithm.best_distance())
+    genetic_algorithm.plot()
+    plt.show(block=True)
---
+++ ma.py
@@ -0,0 +1,189 @@
+import random
+import math
+import matplotlib.pyplot as plt
+from util import City, read_cities, write_cities_and_return_them, generate_cities, path_cost
+
+
+class Particle:
+    def __init__(self, route, cost=None):
+        self.route = route
+        self.pbest = route
+        self.pbest_x = route
+        self.pbest_y = route
+        self.current_cost = cost if cost else self.path_cost()
+        self.pbest_cost = cost if cost else self.path_cost()
+        self.velocity_male = []
+        self.velocity_female = []
+
+    def clear_velocity(self):
+        self.velocity_male.clear()
+        self.velocity_female.clear()
+
+    def update_costs_and_pbest(self):
+        self.current_cost = self.path_cost()
+        if self.current_cost < self.pbest_cost:
+            self.pbest = self.route
+            self.pbest_x = self.route
+            self.pbest_y = self.route
+            self.pbest_cost = self.current_cost
+
+    def path_cost(self):
+        return path_cost(self.route)
+
+
+class PSO:
+
+    def __init__(self, iterations, population_size, gbest_probability=1.0, pbest_probability=1.0, gbest_x_probability=1.0, pbest_x_probability=1.0,gbest_y_probability=1.0, pbest_y_probability=1.0, cities=None):
+        self.cities = cities
+        self.gbest = None
+        self.gbest_x = None
+        self.gbest_y = None
+        self.gcost_iter = []
+        self.gcost_x_iter = []
+        self.gcost_y_iter = []
+        self.iterations = iterations
+        self.population_size = population_size
+        self.particles_x = []
+        self.particles_y = []
+        self.gbest_probability = gbest_probability
+        self.pbest_probability = pbest_probability
+        self.gbest_x_probability = gbest_x_probability
+        self.pbest_x_probability = pbest_x_probability
+        self.gbest_y_probability = gbest_y_probability
+        self.pbest_y_probability = pbest_y_probability
+
+        solutions = self.initial_population()
+        self.particles_x = [Particle(route=solution) for solution in solutions]
+        self.particles_y = [Particle(route=solution) for solution in solutions]
+       
+    def random_route(self):
+        rd = random.sample(self.cities[1:],len(self.cities[1:]))
+        fd = self.cities[:1]
+        return fd+rd
+
+    def initial_population(self):
+        random_population = [self.random_route() for _ in range(self.population_size - 1)]
+        greedy_population = [self.greedy_route(0)]
+        return random_population
+
+    def greedy_route(self, start_index):
+        unvisited = self.cities[:]
+        del unvisited[start_index]
+        # route = self.cities[0]
+        route = [self.cities[start_index]]
+        while len(unvisited):
+            index, nearest_city = min(enumerate(unvisited), key=lambda item: item[1].distance(route[-1]))
+            route.append(nearest_city)
+            del unvisited[index]
+            print(route)
+        return route
+
+    def run(self):
+        self.gbest = min(self.particles_x and self.particles_y, key=lambda p: p.pbest_cost)
+        print(f"Global Best : {self.gbest.pbest_cost}")
+        # plt.ion()
+        # plt.draw()
+        for t in range(self.iterations):
+            self.gbest = min(self.particles_x and self.particles_y, key=lambda p: p.pbest_cost)
+            print(f'Iter {t} Gbest : {pso.gbest.pbest_cost}')
+            if t % 50 == 0:
+                plt.figure(0)
+                plt.plot(pso.gcost_iter, 'g')
+                plt.ylabel('Distance')
+                plt.xlabel('Generation')
+                fig = plt.figure(0)
+                fig.suptitle('pso iter')
+                x_list, y_list = [], []
+                for city in self.gbest.pbest:
+                    x_list.append(city.x)
+                    y_list.append(city.y)
+                x_list.append(pso.gbest.pbest[0].x)
+                y_list.append(pso.gbest.pbest[0].y)
+                fig = plt.figure(1)
+                fig.clear()
+                fig.suptitle(f'pso TSP iter {t}')
+
+                plt.plot(x_list, y_list, 'ro')
+                plt.plot(x_list, y_list, 'g')
+                plt.draw()
+                plt.pause(.001)
+            self.gcost_iter.append(self.gbest.pbest_cost)
+
+            for particle in self.particles_x and self.particles_y:
+                particle.clear_velocity()
+                temp_velocity_male = []
+                temp_velocity_female = []
+                gbest = self.gbest.pbest[:]
+                new_route = particle.route[:]
+                # print(new_route)
+
+                for i in range(len(self.cities)):
+                    if new_route[i] != particle.pbest[i]:
+                        first_route = new_route[0]
+                        # print(first_route)
+                        if new_route[i] != first_route:
+                            swap = (i, particle.pbest.index(new_route[i]), self.pbest_probability)
+                            temp_velocity_male.append(swap)
+                            temp_velocity_female.append(swap)
+                            new_route[swap[0]], new_route[swap[1]] = \
+                                new_route[swap[1]], new_route[swap[0]]
+
+                for i in range(len(self.cities)):
+                    if new_route[i] != gbest[i]:
+                        first_route = new_route[0]
+                        # print(first_route)
+                        if new_route[i] != first_route:
+                            swap = (i, gbest.index(new_route[i]), self.gbest_probability)
+                            temp_velocity_male.append(swap)
+                            temp_velocity_female.append(swap)
+                            gbest[swap[0]], gbest[swap[1]] = gbest[swap[1]], gbest[swap[0]]
+
+                particle.velocity_female = temp_velocity_female
+                particle.velocity_male = temp_velocity_male
+
+                for swap in temp_velocity_male:
+                    if random.random() <= swap[2]:
+                        # if new_route[0] != first_route:
+                        new_route[swap[0]], new_route[swap[1]] = \
+                            new_route[swap[1]], new_route[swap[0]]
+                    # print('swap 0 male')
+                    # print(swap[0])
+                    # print('swap 1 male')
+                    # print(swap[1])
+                    # print('swap 2 male')
+                    # print(swap[2])
+
+                for swap in temp_velocity_female:
+                    if random.random() <= swap[2]:
+                        # if new_route[0] != first_route:
+                        new_route[swap[0]], new_route[swap[1]] = \
+                            new_route[swap[1]], new_route[swap[0]]
+                    # print('swap 0 female')
+                    # print(swap[0])
+                    # print('swap 1 female')
+                    # print(swap[1])
+                    # print('swap 2 female')
+                    # print(swap[2])
+
+                particle.route = new_route
+                particle.update_costs_and_pbest()
+
+
+if __name__ == "__main__":
+    cities = read_cities(64)
+    pso = PSO(iterations=400, population_size=300, pbest_probability=0.9, gbest_probability=0.02,pbest_x_probability=0.9, gbest_x_probability=0.02,pbest_y_probability=0.9, gbest_y_probability=0.02, cities=cities)
+    pso.run()
+    print(f'cost: {pso.gbest.pbest_cost}\t| gbest: {pso.gbest.pbest}')
+
+    x_list, y_list = [], []
+    for city in pso.gbest.pbest:
+        x_list.append(city.x)
+        y_list.append(city.y)
+    x_list.append(pso.gbest.pbest[0].x)
+    y_list.append(pso.gbest.pbest[0].y)
+    fig = plt.figure(1)
+    fig.suptitle('pso TSP')
+
+    plt.plot(x_list, y_list, 'ro')
+    plt.plot(x_list, y_list)
+    plt.show(block=True)
